07e267785dc6dc66edf21aed2423e14bc33f44d5
LineaNotes <anze.premrl@hotmail.com>
2016-03-15 17:45:33 +0100

    Add basic models and modifying context. - add Gas.cs - add GasPrice.cs - modifiy ApplicationDbContext.cs

---
+++ src/WebApplication3/Models/Gas.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication3.Models
+{
+	public class Gas
+	{
+		public int Id { get; set; }
+		public System.DateTime datum { get; set; }
+		public System.DateTime ura_zacetnega_vpisa { get; set; }
+		public System.DateTime ura_koncnega_vpisa { get; set; }
+		public int zacetno_stanje { get; set; }
+		public int koncno_stanje { get; set; }
+		public float proizvedena_kolicina { get; set; }
+		public int zacetno_stanje_cela { get; set; }
+		public int zacetno_stanje_cifra { get; set; }
+		public int koncno_stanje_cela { get; set; }
+		public int koncno_stanje_cifra { get; set; }
+	}
+}
---
+++ src/WebApplication3/Models/GasPrice.cs
@@ -0,0 +1,9 @@
+namespace WebApplication3.Models
+{
+	public class GasPrice
+	{
+		public int Id { get; set; }
+		public System.DateTime GDate { get; set; }
+		public decimal GPrice { get; set; }
+	}
+}
