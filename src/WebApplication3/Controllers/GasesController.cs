b89030c8bbe4856759ef13b6195453f6fc437d02
LineaNotes <anze.premrl@hotmail.com>
2016-03-23 15:50:59 +0100

    Minor commit, Graphs.cshtml needs fixing Some renaming

--- src/WebApplication3/Controllers/GasesController.cs
+++ src/WebApplication3/Controllers/GasesController.cs
@@ -22,7 +22,7 @@ namespace WebApplication3.Controllers
 		}
 
 		// GET: Gases
-		public IActionResult Index(DateTime startDate, DateTime endDate, string test)
+		public IActionResult Index(DateTime startDate, DateTime endDate, string searchBtn)
 		{
 			var gases = from g in _context.Gases select g;
 			var result = "";
@@ -31,7 +31,7 @@ namespace WebApplication3.Controllers
 				? gases.Where(g => g.datum == startDate)
 				: gases.Where(g => g.datum >= startDate && g.datum <= endDate);
 
-			if (!gases.Any() && !string.IsNullOrEmpty(test))
+			if (!gases.Any() && !string.IsNullOrEmpty(searchBtn))
 			{
 				result = "Za izbran razpon ni vnosa";
 			}
@@ -170,22 +170,47 @@ namespace WebApplication3.Controllers
 			public float Proizvod { get; set; }
 		}
 
-		public string GetUtilization()
+		public string GetUtilization(DateTime startDate, DateTime endDate, string executeBtn)
 		{
-			var selection = (from g in _context.Gases select new { g.ura_zacetnega_vpisa, g.ura_koncnega_vpisa }).ToArray();
+			//filtering
+		//var selection = (from g in _context.Gases select new { g.ura_zacetnega_vpisa, g.ura_koncnega_vpisa }).ToArray();
+			if (DateTime.Compare(startDate, new DateTime(2013, 05, 07)) < 0)
+			{
+				startDate = new DateTime(2013, 05, 07);
+				endDate = DateTime.Now;
+			}
+			
+			var gases = from g in _context.Gases select g;
+			var result = "";
+
+			gases = startDate > endDate
+				? gases.Where(g => g.datum == startDate)
+				: gases.Where(g => g.datum >= startDate && g.datum <= endDate);
+
+			if (!gases.Any() && !string.IsNullOrEmpty(executeBtn))
+			{
+				result = "Za izbran razpon ni vnosa";
+			}
+
+			ViewBag.Message = result;
+
+			var selection = gases.AsEnumerable().Select(g => new { g.ura_zacetnega_vpisa, g.ura_koncnega_vpisa }).ToArray();
+
+			// calculations
 			var count = selection.Count();
 
-			var result = new TimeSpan[count];
+			var totalTimeSpan = new TimeSpan[count];
 			var totalMin = 0f;
 
 			for (int i = 0; i < count; i++)
 			{
-				result[i] = selection[i].ura_koncnega_vpisa.Subtract(selection[i].ura_zacetnega_vpisa);
-				totalMin += (float)result[i].TotalMinutes;
+				totalTimeSpan[i] = selection[i].ura_koncnega_vpisa.Subtract(selection[i].ura_zacetnega_vpisa);
+				totalMin += (float)totalTimeSpan[i].TotalMinutes;
 			}
 
-			var totalPercentage = (totalMin /(count * 1440)) * 100;
+			var totalPercentage = (totalMin / (count * 1440)) * 100;
 
+			//assigning to make ready for json serialization
 			object[] obj = new object[2];
 
 			GasUtil gas = new GasUtil();
@@ -194,17 +219,18 @@ namespace WebApplication3.Controllers
 
 			GasUtil gasN = new GasUtil();
 			gasN.Name = "V mirovanju";
-			gasN.Y = 100f - gas.Y;  
+			gasN.Y = 100f - gas.Y;
 
 			obj[0] = gas;
 			obj[1] = gasN;
 
-			string sArrIndented = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver()});
+			string sArrIndented = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
 
 			return sArrIndented;
 		}
 
-		private class GasUtil {
+		private class GasUtil
+		{
 			public string Name { get; set; }
 			public float Y { get; set; }
 		}
