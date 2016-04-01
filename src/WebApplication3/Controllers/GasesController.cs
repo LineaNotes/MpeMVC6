898fda4571fe6285b0ac416f378dbc37999c677b
LineaNotes <anze.premrl@hotmail.com>
2016-04-01 18:59:55 +0200

    Feature: fixed Gases search with ajax functionality

--- src/WebApplication3/Controllers/GasesController.cs
+++ src/WebApplication3/Controllers/GasesController.cs
@@ -9,229 +9,235 @@ using Microsoft.AspNet.Authorization;
 
 namespace WebApplication3.Controllers
 {
-	[Authorize(Roles = "administrator,upravnik")]
-	public class GasesController : Controller
-	{
-		private readonly ApplicationDbContext _context;
+  [Authorize(Roles = "administrator,upravnik")]
+  public class GasesController : Controller
+  {
+    private readonly ApplicationDbContext _context;
 
-		public GasesController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
+    public GasesController(ApplicationDbContext context)
+    {
+      _context = context;
+    }
 
-		// GET: Gases
-		public IActionResult Index(DateTime startDate, DateTime endDate, string searchBtn)
-		{
-			var gases = from g in _context.Gases select g;
-			var result = "";
+    // GET: Gases
+    public IActionResult Index(DateTime startDate, DateTime endDate, string searchBtn)
+    {
+      var gases = from g in _context.Gases select g;
+      var result = "";
 
-			gases = startDate > endDate
-				? gases.Where(g => g.datum == startDate)
-				: gases.Where(g => g.datum >= startDate && g.datum <= endDate);
+      gases = startDate > endDate
+        ? gases.Where(g => g.datum == startDate)
+        : gases.Where(g => g.datum >= startDate && g.datum <= endDate);
 
-			if (!gases.Any() && !string.IsNullOrEmpty(searchBtn))
-			{
-				result = "Za izbran razpon ni vnosa";
-			}
+      if (!gases.Any() && !string.IsNullOrEmpty(searchBtn))
+      {
+        result = "Za izbran razpon ni vnosa";
+      }
 
-			ViewBag.Message = result;
+      ViewBag.Message = result;
 
-			var sum = gases.AsEnumerable().Sum(p => p.proizvedena_kolicina);
-			ViewBag.Summa = sum;
+      var sum = gases.AsEnumerable().Sum(p => p.proizvedena_kolicina);
+      ViewBag.Summa = sum;
 
-			return View(gases);
-		}
+      return View(gases);
+    }
 
-		// GET: Gases/Details/5
-		public IActionResult Details(int? id)
-		{
-			if (id == null)
-			{
-				return HttpNotFound();
-			}
+    // GET: PARTIAL VIEW - Gases/_GasResultTable 
+    public IActionResult GasResultTable()
+    {
+      return PartialView("_GasResultTable");
+    }
 
-			Gas gas = _context.Gases.Single(m => m.Id == id);
-			if (gas == null)
-			{
-				return HttpNotFound();
-			}
+    // GET: Gases/Details/5
+    public IActionResult Details(int? id)
+    {
+      if (id == null)
+      {
+        return HttpNotFound();
+      }
 
-			return View(gas);
-		}
+      Gas gas = _context.Gases.Single(m => m.Id == id);
+      if (gas == null)
+      {
+        return HttpNotFound();
+      }
 
-		// GET: Gases/Create
-		public IActionResult Create()
-		{
-			return View();
-		}
+      return View(gas);
+    }
 
-		// POST: Gases/Create
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Create(Gas gas)
-		{
-			if (ModelState.IsValid)
-			{
-				_context.Gases.Add(gas);
-				_context.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			return View(gas);
-		}
+    // GET: Gases/Create
+    public IActionResult Create()
+    {
+      return View();
+    }
 
-		// GET: Gases/Edit/5
-		public IActionResult Edit(int? id)
-		{
-			if (id == null)
-			{
-				return HttpNotFound();
-			}
+    // POST: Gases/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Gas gas)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Gases.Add(gas);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      return View(gas);
+    }
 
-			Gas gas = _context.Gases.Single(m => m.Id == id);
-			if (gas == null)
-			{
-				return HttpNotFound();
-			}
-			return View(gas);
-		}
+    // GET: Gases/Edit/5
+    public IActionResult Edit(int? id)
+    {
+      if (id == null)
+      {
+        return HttpNotFound();
+      }
 
-		// POST: Gases/Edit/5
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Edit(Gas gas)
-		{
-			if (ModelState.IsValid)
-			{
-				_context.Update(gas);
-				_context.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			return View(gas);
-		}
+      Gas gas = _context.Gases.Single(m => m.Id == id);
+      if (gas == null)
+      {
+        return HttpNotFound();
+      }
+      return View(gas);
+    }
 
-		// GET: Gases/Delete/5
-		[ActionName("Delete")]
-		public IActionResult Delete(int? id)
-		{
-			if (id == null)
-			{
-				return HttpNotFound();
-			}
+    // POST: Gases/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Gas gas)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Update(gas);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      return View(gas);
+    }
 
-			Gas gas = _context.Gases.Single(m => m.Id == id);
-			if (gas == null)
-			{
-				return HttpNotFound();
-			}
+    // GET: Gases/Delete/5
+    [ActionName("Delete")]
+    public IActionResult Delete(int? id)
+    {
+      if (id == null)
+      {
+        return HttpNotFound();
+      }
 
-			return View(gas);
-		}
+      Gas gas = _context.Gases.Single(m => m.Id == id);
+      if (gas == null)
+      {
+        return HttpNotFound();
+      }
 
-		// POST: Gases/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public IActionResult DeleteConfirmed(int id)
-		{
-			Gas gas = _context.Gases.Single(m => m.Id == id);
-			_context.Gases.Remove(gas);
-			_context.SaveChanges();
-			return RedirectToAction("Index");
-		}
+      return View(gas);
+    }
 
-		// GET: Gases/Graphs
-		public IActionResult Graphs()
-		{
-			return View();
-		}
+    // POST: Gases/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+      Gas gas = _context.Gases.Single(m => m.Id == id);
+      _context.Gases.Remove(gas);
+      _context.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-		public string GetAllGasesDateOutput()
-		{
-			var selection = (from g in _context.Gases select new { g.datum, g.proizvedena_kolicina }).ToArray();
-			var list =
-				selection.Select(g => new GasSelection { Datum = Conversion.getTime(g.datum), Proizvod = g.proizvedena_kolicina })
-					.ToArray();
+    // GET: Gases/Graphs
+    public IActionResult Graphs()
+    {
+      return View();
+    }
 
-			object[,] dataFill = new object[list.Length, 2];
-			for (int i = 0; i < list.Length; i++)
-			{
-				dataFill[i, 0] = list[i].Datum;
-				dataFill[i, 1] = list[i].Proizvod;
-			}
+    public string GetAllGasesDateOutput()
+    {
+      var selection = (from g in _context.Gases select new { g.datum, g.proizvedena_kolicina }).ToArray();
+      var list =
+        selection.Select(g => new GasSelection { Datum = Conversion.getTime(g.datum), Proizvod = g.proizvedena_kolicina })
+          .ToArray();
 
-			string sArraySerializedIndented = JsonConvert.SerializeObject(dataFill, Formatting.Indented);
-			return sArraySerializedIndented;
-		}
+      object[,] dataFill = new object[list.Length, 2];
+      for (int i = 0; i < list.Length; i++)
+      {
+        dataFill[i, 0] = list[i].Datum;
+        dataFill[i, 1] = list[i].Proizvod;
+      }
 
-		private class GasSelection
-		{
-			public long Datum { get; set; }
-			public float Proizvod { get; set; }
-		}
+      string sArraySerializedIndented = JsonConvert.SerializeObject(dataFill, Formatting.Indented);
+      return sArraySerializedIndented;
+    }
 
-		public string GetUtilization(DateTime startDate, DateTime endDate, string executeBtn)
-		{
-			//filtering
-			//var selection = (from g in _context.Gases select new { g.ura_zacetnega_vpisa, g.ura_koncnega_vpisa }).ToArray();
-			if (DateTime.Compare(startDate, new DateTime(2013, 05, 07)) < 0)
-			{
-				startDate = new DateTime(2013, 05, 07);
-				endDate = DateTime.Now;
-			}
+    private class GasSelection
+    {
+      public long Datum { get; set; }
+      public float Proizvod { get; set; }
+    }
 
-			var gases = from g in _context.Gases select g;
-			var result = "";
+    public string GetUtilization(DateTime startDate, DateTime endDate, string executeBtn)
+    {
+      //filtering
+      //var selection = (from g in _context.Gases select new { g.ura_zacetnega_vpisa, g.ura_koncnega_vpisa }).ToArray();
+      if (DateTime.Compare(startDate, new DateTime(2013, 05, 07)) < 0)
+      {
+        startDate = new DateTime(2013, 05, 07);
+        endDate = DateTime.Now;
+      }
 
-			gases = startDate > endDate
-				? gases.Where(g => g.datum == startDate)
-				: gases.Where(g => g.datum >= startDate && g.datum <= endDate);
+      var gases = from g in _context.Gases select g;
+      var result = "";
 
-			if (!gases.Any() && !string.IsNullOrEmpty(executeBtn))
-			{
-				result = "Za izbran razpon ni vnosa";
-			}
+      gases = startDate > endDate
+        ? gases.Where(g => g.datum == startDate)
+        : gases.Where(g => g.datum >= startDate && g.datum <= endDate);
 
-			ViewBag.Message = result;
+      if (!gases.Any() && !string.IsNullOrEmpty(executeBtn))
+      {
+        result = "Za izbran razpon ni vnosa";
+      }
 
-			var selection = gases.AsEnumerable().Select(g => new { g.ura_zacetnega_vpisa, g.ura_koncnega_vpisa }).ToArray();
+      ViewBag.Message = result;
 
-			// calculations
-			var count = selection.Count();
+      var selection = gases.AsEnumerable().Select(g => new { g.ura_zacetnega_vpisa, g.ura_koncnega_vpisa }).ToArray();
 
-			var totalTimeSpan = new TimeSpan[count];
-			var totalMin = 0f;
+      // calculations
+      var count = selection.Count();
 
-			for (int i = 0; i < count; i++)
-			{
-				totalTimeSpan[i] = selection[i].ura_koncnega_vpisa.Subtract(selection[i].ura_zacetnega_vpisa);
-				totalMin += (float)totalTimeSpan[i].TotalMinutes;
-			}
+      var totalTimeSpan = new TimeSpan[count];
+      var totalMin = 0f;
 
-			var totalPercentage = (totalMin / (count * 1440)) * 100;
+      for (int i = 0; i < count; i++)
+      {
+        totalTimeSpan[i] = selection[i].ura_koncnega_vpisa.Subtract(selection[i].ura_zacetnega_vpisa);
+        totalMin += (float)totalTimeSpan[i].TotalMinutes;
+      }
 
-			//assigning to make ready for json serialization
-			object[] obj = new object[2];
+      var totalPercentage = (totalMin / (count * 1440)) * 100;
 
-			GasUtil gas = new GasUtil();
-			gas.Name = "V uporabi";
-			gas.Y = totalPercentage;
+      //assigning to make ready for json serialization
+      object[] obj = new object[2];
 
-			GasUtil gasN = new GasUtil();
-			gasN.Name = "V mirovanju";
-			gasN.Y = 100f - gas.Y;
+      GasUtil gas = new GasUtil();
+      gas.Name = "V uporabi";
+      gas.Y = totalPercentage;
 
-			obj[0] = gas;
-			obj[1] = gasN;
+      GasUtil gasN = new GasUtil();
+      gasN.Name = "V mirovanju";
+      gasN.Y = 100f - gas.Y;
 
-			string sArrIndented = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+      obj[0] = gas;
+      obj[1] = gasN;
 
-			return sArrIndented;
-		}
+      string sArrIndented = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
 
-		private class GasUtil
-		{
-			public string Name { get; set; }
-			public float Y { get; set; }
-		}
+      return sArrIndented;
+    }
 
-	}
+    private class GasUtil
+    {
+      public string Name { get; set; }
+      public float Y { get; set; }
+    }
+
+  }
 }
